9942b23a795faeea0c5ecb91b9ce3367d0ef0db4
arnas78 <79596248+arnas78@users.noreply.github.com>
2022-09-27 13:09:32 +0300

    Javascript project version 0.2

--- CA-Project/index.js
+++ CA-Project/index.js
@@ -1,4 +1,21 @@
-const myCar = [
+// VARIABLES
+
+const buttonVilnius = document.querySelector("#btn-vilnius");
+const buttonKaunas = document.querySelector("#btn-kaunas");
+const buttonKlaipeda = document.querySelector("#btn-klaipeda");
+const moviesList = document.querySelector(".movie-list-container");
+const cartSum = document.querySelector(".cart-sum");
+const cartSummary = document.querySelector(".cart-summ");
+const cartContainer = document.querySelector(".cart-container");
+const cartArrowContainer = document.querySelector(".cart-arrow-container");
+const mainEl = document.querySelector("main");
+const cartWrapper = document.querySelector(".cart-wrapper");
+const btnCheckout = document.querySelector(".btn-checkout");
+
+const cities = [buttonKaunas, buttonKlaipeda, buttonVilnius];
+let sum = 0;
+
+const movies = [
   {
     title: "Filmo pavadinimas",
     date: ["12:00", "14:00", "17:00"],
@@ -7,7 +24,7 @@ const myCar = [
     genre: "Siaubo, trileris",
     duration: "2h 20min",
     cast: "Jonas Jonaitis, Petras Petraitis",
-    price: 5.99,
+    price: 8.99,
     imageUrl:
       "https://www.59e59.org/media/filer_public_thumbnails/filer_public/6f/7b/6f7b8c01-950b-415c-97ea-3fd51b57d632/memory_exam_thumbnail_sold_out.jpg__360x540_q85_crop_subsampling-2_upscale.jpg",
   },
@@ -19,7 +36,7 @@ const myCar = [
     genre: "Drama, trileris, kriminalinis",
     duration: "2h 20min",
     cast: "Joaquin Phoenix",
-    price: 5.99,
+    price: 6.99,
     imageUrl:
       "https://www.dramamilk.com/wp-content/uploads/2019/10/Joker-movie-dominates-Korean-box-office-thumbnail.jpg-copy.jpg",
   },
@@ -31,104 +48,379 @@ const myCar = [
     genre: "Drama",
     duration: "2h 20min",
     cast: "Bradd Pitt, Edward Norton",
-    price: 5.99,
+    price: 4.99,
     imageUrl:
       "https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg",
   },
   {
     title: "Scarface",
     date: ["12:00"],
-    city: "Vilnius",
+    city: "Klaipėda",
     desc: "In 1980 Miami, a determined Cuban immigrant takes over a drug cartel and succumbs to greed.",
     genre: "Drama, Kriminalinis",
     duration: "2h 02min",
     cast: "Al Pacino, Michelle Pfeiffer, Steven Bauer",
-    price: 5.99,
+    price: 3.99,
     imageUrl:
       "https://m.media-amazon.com/images/M/MV5BNjdjNGQ4NDEtNTEwYS00MTgxLTliYzQtYzE2ZDRiZjFhZmNlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
   },
 ];
 
-const moviesList = document.querySelector(".movie-list-container");
+let cartMovies = [];
 
-myCar.forEach((singleMovie) => {
-  const movieContainerEl = document.createElement("div");
-  const movieDetailsEl = document.createElement("div");
-  const movieDescEl = document.createElement("h2");
+listMovies();
 
-  const movieGenreContainerEl = document.createElement("div");
-  const movieLengthContainerEl = document.createElement("div");
-  const movieCastContainerEl = document.createElement("div");
-  const moviePriceContainerEl = document.createElement("div");
-  const movieGenreImageEl = document.createElement("i");
-  const movieLengthImageEl = document.createElement("i");
-  const movieCastImageEl = document.createElement("i");
-  const moviePriceImageEl = document.createElement("i");
-  movieGenreImageEl.classList.add("fa-regular", "fa-face-smile");
-  movieLengthImageEl.classList.add("fa-regular", "fa-clock");
-  movieCastImageEl.classList.add("fa-regular", "fa-user");
-  moviePriceImageEl.classList.add("fa-solid", "fa-euro-sign");
+function persistData() {
+  window.localStorage.setItem("cartMovies", JSON.stringify(cartMovies));
+  window.localStorage.setItem("cartPrice", JSON.stringify(sum));
+}
 
-  const movieGenreEl = document.createElement("h3");
-  const movieLengthEl = document.createElement("h3");
-  const movieCastEl = document.createElement("h3");
-  const moviePriceEl = document.createElement("h3");
-  const addToCartBtn = document.createElement("button");
-  const countInput = document.createElement("input");
-
-  const imageContainerEl = document.createElement("div");
-  const imageEl = document.createElement("img");
-
-  const movieInfoEl = document.createElement("div");
-  const movieTitleEl = document.createElement("h1");
-
-  const movieCityEl = document.createElement("h2");
-
-  movieDescEl.textContent = singleMovie.desc;
-  movieGenreEl.textContent = singleMovie.genre;
-  movieLengthEl.textContent = singleMovie.duration;
-  movieCastEl.textContent = singleMovie.cast;
-  moviePriceEl.textContent = singleMovie.price;
-  movieTitleEl.textContent = singleMovie.title;
-
-  movieCityEl.textContent = singleMovie.city;
-  addToCartBtn.textContent = "Į krepšelį";
-
-  countInput.type = "number";
-  countInput.placeholder = "1";
-  imageEl.src = singleMovie.imageUrl;
-
-  movieContainerEl.classList.add("movie-container");
-  movieDetailsEl.classList.add("movie-inside");
-  movieDescEl.classList.add("movie-desc");
-  imageEl.classList.add("movie-image");
-  movieTitleEl.classList.add("movie-title");
-
-  addToCartBtn.classList.add("btn", "btn-cart");
-  countInput.classList.add("input");
-
-  movieGenreContainerEl.append(movieGenreImageEl, movieGenreEl);
-  movieLengthContainerEl.append(movieLengthImageEl, movieLengthEl);
-  movieCastContainerEl.append(movieCastImageEl, movieCastEl);
-  moviePriceContainerEl.append(moviePriceImageEl, moviePriceEl);
-  imageContainerEl.append(imageEl);
-  movieInfoEl.append(movieTitleEl);
-  for (let i = 0; i < singleMovie.date.length; i++) {
-    const movieTimeBtn = document.createElement("button");
-    movieTimeBtn.textContent = singleMovie.date[i];
-    movieTimeBtn.classList.add("btn", "btn-time");
-    movieInfoEl.append(movieTimeBtn);
+window.addEventListener("DOMContentLoaded", () => {
+  const persistedCart = window.localStorage.getItem("cartMovies");
+  const persistedPrice = window.localStorage.getItem("cartPrice");
+  if (persistedCart && persistedPrice) {
+    cartMovies = JSON.parse(persistedCart);
+    sum = JSON.parse(persistedPrice);
+    toDouble();
+    renderCart();
   }
-  movieInfoEl.append(movieCityEl);
-  movieDetailsEl.append(
-    movieDescEl,
-    movieGenreContainerEl,
-    movieLengthContainerEl,
-    movieCastContainerEl,
-    moviePriceContainerEl,
-    addToCartBtn,
-    countInput
-  );
-  movieContainerEl.append(movieDetailsEl, imageContainerEl, movieInfoEl);
-  moviesList.append(movieContainerEl);
 });
+
+// EVENT LISTENERS
+
+cartArrowContainer.addEventListener("click", () => {
+  cartContainer.classList.remove("visible");
+});
+
+// mainEl.addEventListener("click", () => {
+//   cartContainer.classList.remove("visible");
+// });
+
+cartSummary.addEventListener("click", () => {
+  cartContainer.classList.add("visible");
+  cartWrapper.textContent = "";
+  renderCart();
+});
+
+cities.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.classList.contains("btn-active")) {
+      moviesList.textContent = "";
+      button.classList.remove("btn-active");
+      listMovies();
+    } else {
+      button.classList.add("btn-active");
+      moviesList.textContent = "";
+      listMovies();
+    }
+  });
+});
+
+// FUNCTIONS
+
+function renderMovie(city) {
+  movies.forEach((singleMovie) => {
+    let timeArr = [];
+    if (singleMovie.city === city) {
+      const movieContainerEl = document.createElement("div");
+      const movieDetailsEl = document.createElement("div");
+      const movieDescEl = document.createElement("h2");
+      const movieGenreContainerEl = document.createElement("div");
+      const movieLengthContainerEl = document.createElement("div");
+      const movieCastContainerEl = document.createElement("div");
+      const moviePriceContainerEl = document.createElement("div");
+      const movieGenreImageEl = document.createElement("i");
+      const movieLengthImageEl = document.createElement("i");
+      const movieCastImageEl = document.createElement("i");
+      const moviePriceImageEl = document.createElement("i");
+      const movieGenreEl = document.createElement("h3");
+      const movieLengthEl = document.createElement("h3");
+      const movieCastEl = document.createElement("h3");
+      const moviePriceEl = document.createElement("h3");
+      const addToCartBtn = document.createElement("button");
+      const countInput = document.createElement("input");
+      const imageContainerEl = document.createElement("div");
+      const imageEl = document.createElement("img");
+      const movieInfoEl = document.createElement("div");
+      const movieTitleEl = document.createElement("h1");
+      const movieCityEl = document.createElement("h2");
+      const movieTimeContainer = document.createElement("div");
+
+      movieDescEl.textContent = singleMovie.desc;
+      movieGenreEl.textContent = singleMovie.genre;
+      movieLengthEl.textContent = singleMovie.duration;
+      movieCastEl.textContent = singleMovie.cast;
+      moviePriceEl.textContent = singleMovie.price;
+      movieTitleEl.textContent = singleMovie.title;
+
+      movieCityEl.textContent = singleMovie.city;
+      addToCartBtn.textContent = "Į krepšelį";
+
+      countInput.type = "number";
+      countInput.value = 1;
+      imageEl.src = singleMovie.imageUrl;
+
+      movieContainerEl.classList.add("movie-container");
+      movieDetailsEl.classList.add("movie-inside");
+      movieDescEl.classList.add("movie-desc");
+      imageEl.classList.add("movie-image");
+      movieTitleEl.classList.add("movie-title");
+      addToCartBtn.classList.add("btn", "btn-cart");
+      countInput.classList.add("input");
+      movieGenreImageEl.classList.add("fa-regular", "fa-face-smile");
+      movieLengthImageEl.classList.add("fa-regular", "fa-clock");
+      movieCastImageEl.classList.add("fa-regular", "fa-user");
+      moviePriceImageEl.classList.add("fa-solid", "fa-euro-sign");
+
+      movieGenreContainerEl.append(movieGenreImageEl, movieGenreEl);
+      movieLengthContainerEl.append(movieLengthImageEl, movieLengthEl);
+      movieCastContainerEl.append(movieCastImageEl, movieCastEl);
+      moviePriceContainerEl.append(moviePriceImageEl, moviePriceEl);
+      imageContainerEl.append(imageEl);
+      movieInfoEl.append(movieTitleEl);
+
+      for (let i = 0; i < singleMovie.date.length; i++) {
+        let date = singleMovie.date[i];
+        const movieTimeBtn = document.createElement("button");
+        movieTimeBtn.textContent = date;
+        movieTimeBtn.classList.add("btn", "btn-time");
+        movieTimeContainer.append(movieTimeBtn);
+        timeArr.push(movieTimeBtn);
+      }
+      movieInfoEl.append(movieTimeContainer);
+
+      for (let i = 0; i < timeArr.length; i++) {
+        let time = timeArr[i];
+
+        time.addEventListener("click", () => {
+          if (time.classList.contains("btn-active")) {
+            time.classList.remove("btn-active");
+          } else {
+            // Deactivates every active button, so only one button can be active at a time
+            for (let j = 0; j < timeArr.length; j++) {
+              let time2 = timeArr[j];
+              if (time2.classList.contains("btn-active")) {
+                time2.classList.remove("btn-active");
+              }
+            }
+            time.classList.add("btn-active");
+          }
+        });
+      }
+
+      movieInfoEl.append(movieCityEl);
+      movieDetailsEl.append(
+        movieDescEl,
+        movieGenreContainerEl,
+        movieLengthContainerEl,
+        movieCastContainerEl,
+        moviePriceContainerEl,
+        addToCartBtn,
+        countInput
+      );
+      movieContainerEl.append(movieDetailsEl, imageContainerEl, movieInfoEl);
+      moviesList.append(movieContainerEl);
+
+      addToCartBtn.addEventListener("click", () => {
+        let chosenTime = "";
+        for (let i = 0; i < timeArr.length; i++) {
+          if (timeArr[i].classList.contains("btn-active")) {
+            chosenTime = timeArr[i].textContent;
+          }
+        }
+
+        if (chosenTime !== "") {
+          let price = countInput.value * singleMovie.price;
+          sum += price;
+          toDouble();
+          persistData();
+          if (
+            cartMovies.some(
+              (movie) =>
+                movie.title === singleMovie.title && movie.date === chosenTime
+            )
+          ) {
+            let movieAmount = Number(
+              cartMovies.find(
+                (movie) =>
+                  movie.title === singleMovie.title && movie.date === chosenTime
+              ).amount
+            );
+            cartMovies.find(
+              (movie) =>
+                movie.title === singleMovie.title && movie.date === chosenTime
+            ).amount = movieAmount += Number(countInput.value);
+            cartWrapper.textContent = "";
+            renderCart();
+          } else {
+            const cartItem = {
+              ["title"]: singleMovie.title,
+              ["city"]: singleMovie.city,
+              ["date"]: chosenTime,
+              ["price"]: singleMovie.price,
+              ["amount"]: countInput.value,
+              ["imageUrl"]: singleMovie.imageUrl,
+            };
+            cartMovies.push(cartItem);
+            cartWrapper.textContent = "";
+            renderCart();
+          }
+        } else {
+          alert("PASIRINKITE SEANSO LAIKĄ");
+        }
+      });
+    }
+  });
+}
+
+function toDouble() {
+  if (sum == 0) {
+    cartSum.textContent = "0.00 €";
+  } else {
+    cartSum.textContent = Math.round(sum * 100) / 100 + " €";
+  }
+}
+
+function renderCart() {
+  if (cartMovies.length == 0) {
+    cartWrapper.textContent = "";
+    const cartItemContainer = document.createElement("div");
+    cartItemContainer.className = "cart-item-empty";
+    const cartItemName = document.createElement("h2");
+    cartItemName.textContent = "Jūsų krepšelis yra tuščias!";
+    cartItemContainer.append(cartItemName);
+    cartWrapper.append(cartItemContainer);
+    btnCheckout.classList.add("hidden");
+  } else {
+    cartMovies.forEach((cartMovie) => {
+      btnCheckout.classList.remove("hidden");
+      const cartItemContainer = document.createElement("div");
+      const cartImage = document.createElement("img");
+      const cartItemDetails = document.createElement("div");
+      const cartItemName = document.createElement("h2");
+      const cartItemCity = document.createElement("p");
+      const cartItemTime = document.createElement("p");
+      const cartItemPrice = document.createElement("h2");
+      const cartItemAmountContainer = document.createElement("div");
+      const cartItemAmount = document.createElement("p");
+      const cartCountInput = document.createElement("input");
+      const cartItemDeleteEl = document.createElement("i");
+
+      cartImage.src = cartMovie.imageUrl;
+      cartItemPrice.textContent = cartMovie.price + " €";
+      cartItemName.textContent = cartMovie.title;
+      cartItemCity.textContent = cartMovie.city;
+      cartItemTime.textContent = cartMovie.date;
+
+      cartItemAmount.textContent = "Kiekis:";
+
+      cartCountInput.type = "number";
+      cartCountInput.value = cartMovie.amount;
+      cartCountInput.onchange = function () {
+        let temp = cartMovie.amount;
+        cartMovie.amount = cartCountInput.value;
+        let price = 1 * cartMovie.price;
+        if (temp < cartCountInput.value) {
+          sum += price;
+        } else {
+          sum -= price;
+        }
+        toDouble();
+        persistData();
+      };
+
+      cartItemContainer.className = "cart-item";
+      cartImage.className = "cart-image";
+      cartItemDetails.className = "cart-item-details";
+      cartItemAmountContainer.className = "cart-amount";
+      cartCountInput.className = "input-cart";
+      cartItemDeleteEl.classList.add("fa-solid", "fa-x");
+
+      cartItemAmountContainer.append(cartItemAmount, cartCountInput);
+      cartItemDetails.append(
+        cartItemName,
+        cartItemCity,
+        cartItemTime,
+        cartItemPrice,
+        cartItemAmountContainer
+      );
+      cartItemContainer.append(cartImage, cartItemDetails, cartItemDeleteEl);
+
+      cartWrapper.append(cartItemContainer);
+
+      cartItemDeleteEl.addEventListener("click", () => {
+        cartMovies.splice(cartMovies.indexOf(cartMovie), 1);
+        let price = cartCountInput.value * cartMovie.price;
+        sum -= price;
+        toDouble();
+        persistData();
+        cartWrapper.textContent = "";
+        renderCart();
+      });
+    });
+    persistData();
+  }
+}
+
+function adjustCount() {
+  console.log("asd");
+}
+
+function listMovies() {
+  if (
+    !buttonKaunas.classList.contains("btn-active") &&
+    !buttonVilnius.classList.contains("btn-active") &&
+    !buttonKlaipeda.classList.contains("btn-active")
+  ) {
+    renderMovie("Vilnius");
+    renderMovie("Kaunas");
+    renderMovie("Klaipėda");
+  } else if (
+    !buttonKaunas.classList.contains("btn-active") &&
+    !buttonVilnius.classList.contains("btn-active") &&
+    buttonKlaipeda.classList.contains("btn-active")
+  ) {
+    renderMovie("Klaipėda");
+  } else if (
+    !buttonKaunas.classList.contains("btn-active") &&
+    buttonVilnius.classList.contains("btn-active") &&
+    !buttonKlaipeda.classList.contains("btn-active")
+  ) {
+    renderMovie("Vilnius");
+  } else if (
+    buttonKaunas.classList.contains("btn-active") &&
+    !buttonVilnius.classList.contains("btn-active") &&
+    !buttonKlaipeda.classList.contains("btn-active")
+  ) {
+    renderMovie("Kaunas");
+  } else if (
+    !buttonKaunas.classList.contains("btn-active") &&
+    buttonVilnius.classList.contains("btn-active") &&
+    buttonKlaipeda.classList.contains("btn-active")
+  ) {
+    renderMovie("Vilnius");
+    renderMovie("Klaipėda");
+  } else if (
+    buttonKaunas.classList.contains("btn-active") &&
+    buttonVilnius.classList.contains("btn-active") &&
+    !buttonKlaipeda.classList.contains("btn-active")
+  ) {
+    renderMovie("Vilnius");
+    renderMovie("Kaunas");
+  } else if (
+    buttonKaunas.classList.contains("btn-active") &&
+    !buttonVilnius.classList.contains("btn-active") &&
+    buttonKlaipeda.classList.contains("btn-active")
+  ) {
+    renderMovie("Kaunas");
+    renderMovie("Klaipėda");
+  } else if (
+    buttonKaunas.classList.contains("btn-active") &&
+    buttonVilnius.classList.contains("btn-active") &&
+    buttonKlaipeda.classList.contains("btn-active")
+  ) {
+    renderMovie("Kaunas");
+    renderMovie("Klaipėda");
+    renderMovie("Vilnius");
+  }
+}
